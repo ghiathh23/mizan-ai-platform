26a8678212884f0666872be2d4cd9b4acdec25b9
ghiathh23 <ghiathmofeedalsalami2023@gmail.com>
2026-09-18 22:40:56 +0300

    feat(app): activate offline sync bootstrap

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import { registerOfflineSync } from './offline/offlineBootstrap'
 import './styles.css'
 
+registerOfflineSync()
 createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>)
